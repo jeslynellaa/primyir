59b048db2d58c7f54455f2f04ec09827f123a322
jeslynellaa <jeslyndanielleboroc.ella@bicol-u.edu.ph>
2023-01-18 23:18:41 +0800

    added ph locations seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $this->call(RegionsTableSeeder::class);
+        $this->call(ProvincesTableSeeder::class);
+        $this->call(CityMunicipalitiesTableSeeder::class);
+        $this->call(BarangaysTableSeeder::class);
     }
 }
