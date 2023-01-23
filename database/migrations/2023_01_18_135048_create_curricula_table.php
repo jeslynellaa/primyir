63e8da8b2b10e2f0c9f074469c14965989c8ab2b
jeslynellaa <jeslyndanielleboroc.ella@bicol-u.edu.ph>
2023-01-23 15:13:56 +0800

    added curriculum table and model

---
+++ database/migrations/2023_01_18_135048_create_curricula_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCurriculaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('curricula', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 30);
+            $table->string('acronym', 30);
+            $table->string('description', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('curricula');
+    }
+}
