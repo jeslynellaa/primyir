b7fa3ba43d7c5a7d0522462b8e65808179f73714
jeslynellaa <jeslyndanielleboroc.ella@bicol-u.edu.ph>
2023-02-09 00:36:34 +0800

    Login controller edits

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -26,7 +27,17 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    //protected $redirectTo = RouteServiceProvider::HOME;
+
+    protected function authenticated($user) {
+
+        if ($user->owner_type == 'A') {
+            return redirect('/admin');
+        }
+        else if ($user->owner_type == 'T') {
+            return redirect('/faculty');
+        }
+   }
 
     /**
      * Create a new controller instance.
