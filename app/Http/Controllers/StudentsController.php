597773f8262fa38087320420a545997bd94b41e4
jeslynellaa <jeslyndanielleboroc.ella@bicol-u.edu.ph>
2023-02-20 00:36:06 +0800

    new functions for view, edit, update, store

--- app/Http/Controllers/StudentsController.php
+++ app/Http/Controllers/StudentsController.php
@@ -11,6 +11,9 @@ use App\Models\Address;
 use App\Models\Student;
 use App\Models\Section;
 use App\Models\StudentSchoolyear;
+use App\Models\StudentSubjClass;
+use App\Models\StudentSubjGrade;
+use Illuminate\Validation\Rule;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -29,10 +32,7 @@ class StudentsController extends Controller
         $student_users = DB::table('users')
             ->join('students', 'students.user_id', '=', 'users.id')
             ->where('users.owner_type', 'S')
-            ->join('student_schoolyears', 'student_schoolyears.student_id', '=', 'students.id')
-            ->join('sections', 'student_schoolyears.section_id', '=', 'sections.id')
-            ->select('students.id as stud_id', 'students.LRN_no', 'students.curriculum_id', 'users.*', 'sections.name', 'sections.grade_level')
-            ->orderBy('grade_level')
+            ->select('students.id as stud_id', 'students.LRN_no', 'students.curriculum_id', 'users.*')
             ->orderBy('lastName', 'ASC')
             ->get()->paginate(2);
         
@@ -122,16 +122,19 @@ class StudentsController extends Controller
             ->select('students.id as stud_id', 'students.LRN_no', 'students.curriculum_id', 
                 'users.givenName', 'users.middleName', 'users.lastName', 
                 'sections.name', 'sections.grade_level',
-                'student_schoolyears.status',
+                'student_schoolyears.status', 'student_schoolyears.id as syid',
                 'schoolyears.year_start', 'schoolyears.year_end')
+            ->orderBy('year_start')
             ->get();
         $student = DB::table('users')
             ->join('students', 'students.user_id', '=', 'users.id')
             ->where('students.id', $stud_id)
             ->select('users.givenName', 'users.middleName', 'users.lastName', 'students.id as stud_id')
             ->first();
+        $student_model = \App\Models\Student::find($stud_id);
+        //dd($student_model);
         //dd($student_records);
-        return view('admin.students.view', compact('student_records', 'student'));
+        return view('admin.students.view', compact('student_records', 'student', 'student_model'));
     }
 
     public function create_enrollment(User $user, $stud_id)
@@ -173,6 +176,95 @@ class StudentsController extends Controller
         return redirect()->back()->with("success","New Student Enrollment Record Created Successfully!");
     }
     
+    public function enroll_show($stud_id, $syid)
+    {
+        //dd($syid);
+        $schoolyear = \App\Models\StudentSchoolyear::find($syid);
+        $student_model = \App\Models\Student::find($stud_id);
+
+        $student_records = DB::table('students')
+        ->join('student_schoolyears', 'students.id', '=', 'student_schoolyears.student_id')
+        ->where('student_schoolyears.schoolyear_id', $syid)
+        ->join('student_subj_classes', 'student_schoolyears.id', '=', 'student_subj_classes.student_schoolyear_id')
+        ->join('student_subj_grades', 'student_subj_classes.id', '=', 'student_subj_grades.student_subj_class_id')
+        ->get();
+        return view('admin.students.view-record', compact('student_model', 'schoolyear', 'student_records'));
+    }
+
+    public function enroll_add($stud_id, $syid)
+    {
+        $schoolyear = \App\Models\StudentSchoolyear::find($syid);
+        $student_model = \App\Models\Student::find($stud_id);
+
+        $grade = $schoolyear->Section->grade_level;
+        $section = $schoolyear->Section->name;
+
+        $student_records = DB::table('students')
+        ->join('student_schoolyears', 'students.id', '=', 'student_schoolyears.student_id')
+        ->where('student_schoolyears.schoolyear_id', $syid)
+        ->join('student_subj_classes', 'student_schoolyears.id', '=', 'student_subj_classes.student_schoolyear_id')
+        ->join('student_subj_grades', 'student_subj_classes.id', '=', 'student_subj_grades.student_subj_class_id')
+        ->get();
+
+        $assigned_subs = DB::table('subjects')
+        ->join('subject_classes', 'subjects.id', '=', 'subject_classes.subject_id')
+        ->join('student_subj_classes', 'student_subj_classes.subject_class_id', '=', 'subject_classes.id')
+        ->select('subjects.name as subject', 'subject_classes.id as subclass_id')
+        ->get();
+
+        $avail_subs = DB::table('subjects')
+        ->join('subject_classes', 'subjects.id', '=', 'subject_classes.subject_id')
+        ->join('student_subj_classes', 'student_subj_classes.subject_class_id', '=', 'subject_classes.id', 'left outer')
+        ->where('subjects.grade_level', $grade)
+        ->select('subjects.grade_level','student_id as student', 'subjects.name as subject', 'subject_classes.id as subclass_id')
+        ->whereNull('student_id')
+        ->get();
+        //dd($avail_subs);
+
+        // $subjects = DB::table('subjects')
+        // ->join('subject_classes', 'subjects.id', '=', 'subject_classes.subject_id')
+        // ->join('sections', 'sections.id', '=', 'subject_classes.section_id')
+        // ->where('sections.grade_level', $grade)
+        // ->select('subjects.name as subject', 'subject_classes.id as subclass_id')
+        // ->get();
+
+        //dd($subjects);
+        return view('admin.students.add-subject', compact('student_model', 'grade','section','schoolyear', 'student_records', 'assigned_subs','avail_subs'));
+    }
+
+    public function enroll_store(Request $request, User $user, $stud_id, $syid)
+    {
+        $this->authorize('create', $user);
+
+        $data = request()->validate([
+            'student' => ['required','integer'],
+            'student_sy' => ['required','integer'],
+            'subjects' => 'required'
+        ]);
+        //dd($request->all());
+        foreach ($request->subjects as $key => $value) {
+            \App\Models\StudentSubjClass::create([
+                'student_id' => $data['student'],
+                'student_schoolyear_id' => $data['student_sy'],
+                'subject_class_id' => $value
+            ]);
+        }
+            // $newStudentSubClass = \App\Models\StudentSubjClass::create([
+            //     'student_id' => $data['student'],
+            //     'subject_class_id' => $data['subjects[$key]'],
+            //     'student_schoolyear_id' => $data['student_sy'],
+            // ]);
+        
+        // 
+
+        // $newStudentSY = \App\Models\StudentSchoolyear::create([
+        //     'student_id' => $data['student_id'],
+        //     'schoolyear_id' => $data['schoolyear'],
+        //     'section_id' => $data['section'],
+        //     'status' => $data['status']
+        // ]);
+        return redirect()->back()->with("success","Subjects Added Successfully!");
+    }
 
     public function getSections(Request $request){
         $sections = DB::table('sections')
