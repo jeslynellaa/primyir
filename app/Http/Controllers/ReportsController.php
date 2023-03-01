f26119e86cfd44ae8e8aebd5768b715cb2582b8a
jeslynellaa <jeslyndanielleboroc.ella@bicol-u.edu.ph>
2023-03-02 02:17:55 +0800

    working sf1

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -7,6 +7,9 @@ use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use App\Models\User;
 use App\Models\Section;
+use App\Models\Schoolyear;
+use App\Models\StudentSchoolyear;
+use App\Models\StudentRegister;
 use App\Models\Teacher;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -24,12 +27,33 @@ class ReportsController extends Controller
     }
 
     public function sf1(){
-        return view('admin.reports.sf1_view');
+        $currentSY = Schoolyear::where('isCurrent', true)->first();
+        //dd($currentSY);
+
+        $section_sy = DB::table('sections')
+        ->crossJoin('schoolyears')
+        ->orderBy('schoolyears.year_start', 'DESC')
+        ->orderBy('sections.grade_level', 'ASC')
+        ->select('schoolyears.id as sy', 'year_start', 'year_end', 'isCurrent',
+            'sections.id as section', 'name','grade_level', 'adviser')
+        ->get()->paginate(20);
+
+        //dd($section_sy);
+        return view('admin.reports.sf1.view', compact('section_sy'));
     }
 
-    public function sf1_create(User $user){
+    public function sf1_create(User $user, $studentsy){
         $this->authorize('create', $user);
+        //dd($studentsy);
 
+        $studSY = StudentSchoolyear::where('id', $studentsy)->first();
+        //dd($studSY->StudentRegister);
+
+        $studentReg = StudentRegister::where([
+            'student_id' => $studSY->student_id,
+            'schoolyear_id' => $studSY->schoolyear_id
+        ])->first();
+        //dd($studentReg);
         $schoolyears = DB::table('schoolyears')
             ->orderBy('year_start')
             ->get();
@@ -43,8 +67,12 @@ class ReportsController extends Controller
             ->orderBy('lastName', 'ASC')
             ->get();
         //dd($schoolyears);
-
-        return view('admin.reports.sf1_create', compact('schoolyears'));
+        
+        if(isset($studentReg)){
+            return view('admin.reports.sf1.edit', compact('schoolyears', 'studSY', 'studentReg'));
+        }else{
+            return view('admin.reports.sf1.create', compact('schoolyears', 'studSY'));
+        }
     }
 
     public function sf1_store(Request $request, User $user)
@@ -96,6 +124,95 @@ class ReportsController extends Controller
         return redirect()->back()->with("success","New SF1 Record Created!");
     }
 
+    public function sf1_update(Request $request, User $user, $id)
+    {
+        $studentReg = StudentRegister::where('id', $id)->first();
+        //dd($studentReg);
+        // Validation of input
+        $data = request()->validate([
+            'schoolyear' => ['required'],
+            'grade_lvl' => ['required'],
+            'section' => ['required'],
+            'student' => ['required'],
+            'age' => ['required'],
+            'mother_tongue' => ['required', 'string'],
+            'IP_ethnicGroup' => '',
+            'father_givenName' => ['required', 'string'],
+            'father_middleName' => '',
+            'father_lastName' => ['required', 'string'],
+            'mother_givenName' => ['required', 'string'],
+            'mother_middleName' => '',
+            'mother_lastName' => ['required', 'string'],
+            'guardian_givenName' => '',
+            'guardian_middleName' => '',
+            'guardian_lastName' => '',
+            'guardian_relationship' => '',
+            'contact' => '',
+            'remarks' => '',
+        ]);        
+
+        $studentReg->student_id = $data['student'];
+        $studentReg->section_id = $data['section'];
+        $studentReg->age = $data['age'];
+        $studentReg->mother_tongue = $data['mother_tongue'];
+        $studentReg->father_givenName = $data['father_givenName'];
+        $studentReg->father_middleName = $data['father_middleName'];
+        $studentReg->father_lastName = $data['father_lastName'];
+        $studentReg->mother_givenName = $data['mother_givenName'];
+        $studentReg->mother_middleName = $data['mother_middleName'];
+        $studentReg->mother_lastName = $data['mother_lastName'];
+        $studentReg->guardian_givenName = $data['guardian_givenName'];
+        $studentReg->guardian_middleName = $data['guardian_middleName'];
+        $studentReg->guardian_lastName = $data['guardian_lastName'];
+        $studentReg->guardian_relationship = $data['guardian_relationship'];
+        $studentReg->parent_guardian_contactNo = $data['contact'];
+        $studentReg->remarks = $data['remarks'];
+        $studentReg->schoolyear_id = $data['schoolyear'];
+        $studentReg->save();
+
+        return redirect()->back()->with("success","Changes Saved Successfully!");
+    }
+
+    public function sf1_show(User $user, $sy, $section_id){
+        $schoolyear = Schoolyear::where('id','=', $sy)->first();
+        //dd($schoolyear);
+        $section = Section::where('id','=', $section_id)->first();
+        //dd($section);
+        
+        // $students = \App\Models\StudentSchoolyear::where([
+        //     'section_id' => $section_id,
+        //     'schoolyear_id' => $sy
+        // ])->get();
+        
+        $students = DB::table('student_schoolyears')
+        ->join('students', 'student_id', 'students.id')
+        ->join('users', 'user_id', 'users.id')
+        ->join('sections', 'section_id', 'sections.id')
+            ->where('student_schoolyears.section_id', $section_id)
+            ->where('student_schoolyears.schoolyear_id', $sy)
+            ->select('student_schoolyears.id as studentsy', 'students.*', 'users.*', 'sections.*')
+            ->get();
+        //dd($students);
+
+        //dd($section_sy);
+        return view('admin.reports.sf1.show', compact('schoolyear','section','students'));
+    }
+
+    
+    public function generate_sf1(User $user, $sy, $section_id){
+        $schoolyear = Schoolyear::where('id','=', $sy)->first();
+        //dd($schoolyear);
+        $section = Section::where('id','=', $section_id)->first();
+
+        $records = StudentRegister::where([
+            'section_id' => $section_id,
+            'schoolyear_id' => $sy
+        ])->get();
+        //dd($records);
+        return view('sf_pdf.sf1', compact('schoolyear','section','records'));
+    }
+
+
     public function sf2(){
         return view('sf_pdf.sf2');
     }
@@ -103,6 +220,7 @@ class ReportsController extends Controller
     public function sf4(){
         return view('sf_pdf.sf4');
     }
+    
     public function sf6(){
         return view('admin.reports.sf6');
     }
---
+++ resources/views/admin/reports/sf1/view.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.admin-app')
+
+@section('content')
+    <div class="lists_wrap">
+
+        <div class="well">
+            <div class="well_top" style="align-items:center">
+                <h2 style="align:left">List of SF 1 Records</h2>
+            </div>
+
+            <form>  					
+                <table id="example" class="table table-striped" cellspacing="0">
+                    <thead>
+                        <tr>
+                            <th style="width=10% align=left"><input type="checkbox" name="chkall" id="chkall" onclick="return checkall('selector[]');">No.</th>
+                            <th>Grade</th>
+                            <th>Section</th>
+                            <th>School Year</th>
+                            <th>Options</th>
+                        </tr>	
+                    </thead>
+
+                    <tbody>
+                        @foreach($section_sy as $section)
+                            <tr>
+                                <td>{{ $loop->index + 1}}</td>
+                                <td>{{ $section->grade_level }}</td>
+                                <td>{{ $section->name }}</td>
+                                <td>{{ $section->year_start }} - {{ $section->year_end }}</td>
+                                <td><a href="/admin/reports/sf1/{{$section->sy}}/{{$section->section}}">View</a></td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </form>
+        </div>
+        <span>
+            {{$section_sy->links()}}
+        </span>
+            
+        <style>
+            .w-5{
+                display: none;
+            }
+        </style>
+    </div>
+@endsection
