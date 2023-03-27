1222acb50079c8e935bb823c12be2ded135e0f95
jeslynellaa <jeslyndanielleboroc.ella@bicol-u.edu.ph>
2023-03-28 02:00:32 +0800

    student side functions

--- app/Http/Controllers/GradesController.php
+++ app/Http/Controllers/GradesController.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use App\Models\User;
 use App\Models\Section;
+use App\Models\Student;
 use App\Models\Schoolyear;
 use App\Models\SubjectClass;
 use App\Models\StudentSubjClass;
@@ -84,7 +85,6 @@ class GradesController extends Controller
                         'subject_class_id' => $id,
                         'student_schoolyear_id' => $currentSY->id
                     ])->get();
-                    
               break;
             case 2:
                 $student_grades = DB::table('student_subj_classes')
@@ -143,5 +143,53 @@ class GradesController extends Controller
         }
 
         return redirect()->back()->with("success","Grades Saved Successfully!");
+    }
+
+    
+    public function student_index(User $user){
+        $user = Auth::user();
+        $student = $user->Student;
+        //dd( $faculty->Teacher->id);
+        
+        // Joining sections, teachers, and users into section_teachers for displaying in sections table
+        $student_records = DB::table('users')
+            ->join('students', 'students.user_id', '=', 'users.id')
+            ->where('students.id', $student->id)
+            ->join('student_schoolyears', 'student_schoolyears.student_id', '=', 'students.id')
+            ->join('sections', 'student_schoolyears.section_id', '=', 'sections.id')
+            ->join('schoolyears', 'student_schoolyears.schoolyear_id', '=', 'schoolyears.id')
+            ->select('students.id as stud_id', 'students.LRN_no', 'students.curriculum_id', 
+                'users.givenName', 'users.middleName', 'users.lastName', 
+                'sections.name', 'sections.grade_level',
+                'student_schoolyears.status', 'student_schoolyears.id as syid',
+                'schoolyears.year_start', 'schoolyears.year_end')
+            ->orderBy('year_start')
+            ->get();
+
+        return view ('students.grades.index', compact('student','student_records'));
+    }
+
+    public function student_show($stud_id, $syid)
+    {
+        //dd($syid);
+        $schoolyear = \App\Models\StudentSchoolyear::find($syid);
+        $student_model = \App\Models\Student::find($stud_id);
+
+        // $student_records = DB::table('students')
+        // ->join('student_schoolyears', 'students.id', '=', 'student_schoolyears.student_id')
+        // ->where('student_schoolyears.schoolyear_id', $syid)
+        // ->join('student_subj_classes', 'student_schoolyears.id', '=', 'student_subj_classes.student_schoolyear_id')
+        // ->join('student_subj_grades', 'student_subj_classes.id', '=', 'student_subj_grades.student_subj_class_id')
+        // ->get();
+
+        $grades = Student::join('student_subj_classes','students.id','student_subj_classes.student_id')
+            ->join('student_subj_grades', 'student_subj_classes.id', 'student_subj_grades.student_subj_class_id')
+            ->join('subject_classes', 'student_subj_classes.subject_class_id', 'subject_classes.id')
+            ->join('subjects', 'subject_classes.subject_id', 'subjects.id')
+            ->where('student_subj_classes.student_schoolyear_id', $syid)
+            // ->where('id', $stud_id)
+            ->get();
+        // dd($grades);
+        return view('students.grades.view-record', compact('student_model', 'schoolyear', 'grades'));
     }
 }
